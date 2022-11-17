f9467e9a35d57dbc928d167da71f02703130639a
Mayur Upadhayay <mayur123a@gmail.com>
2022-11-17 22:09:12 +0530

    feat: Added example type that returns tuple length

---
+++ basic/TupleLength.ts
@@ -0,0 +1,11 @@
+/**
+ * Implement a generic that returns the cardinality of tupe type T
+ */
+
+type tesla = ['tesla', 'model 3', 'model X', 'model Y'];
+type spaceX = ['FALCON 9', 'FALCON HEAVY', 'DRAGON', 'STARSHIP', 'HUMAN SPACEFLIGHT'];
+
+type Length<T extends readonly any[]> = T["length"];
+
+type teslaLength = Length<tesla>; // returns 4
+type spaceXLength = Length<spaceX> // returns 5
