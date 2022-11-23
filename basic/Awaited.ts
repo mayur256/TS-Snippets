5cf14e9a6feecaa924306a92958ead4eef245abd
Mayur Upadhayay <mayur123a@gmail.com>
2022-11-23 23:10:32 +0530

    feat: Implemented Custom Awaited type

--- basic/Awaited.ts
+++ basic/Awaited.ts
@@ -7,3 +7,11 @@ type NumericPromise = Promise<number>;
 type Numeric = Awaited<NumericPromise>;
 
 // const number: Numeric = "5"; // throws error type string is not assignable to number
+
+/**
+ * Implement a custom type that returns the type wrapped by a Promise
+ * e.g return ExampleType from Promise<ExampleType>
+ */
+type CustomAwaited<T extends Promise<unknown>> = T extends Promise<infer K> ? K : never;
+
+export { };
