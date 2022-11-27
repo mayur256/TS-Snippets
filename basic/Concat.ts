0302c33c9d867545f99de3e14e347bf0827dd291
Mayur Upadhayay <mayur123a@gmail.com>
2022-11-27 23:40:38 +0530

    feat: Implemented a type that simulates Array.prototype.concat

---
+++ basic/Concat.ts
@@ -0,0 +1,12 @@
+/**
+ * Implement the JavaScript Array.concat function in the type system. A type takes the two arguments.
+ * The output should be a new array that includes inputs in ltr order
+ * e.g
+ * type Result = Concat<[1], [2]> // expected to be [1, 2]
+ */
+
+type Concat<X extends Array<any>, Y extends Array<any>> = [...X, ...Y];
+
+type Result = Concat<[1], [2]>;
+
+export default {};
