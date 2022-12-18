9253b4b01fabf0b2b3749b2348e06456b13242c4
Mayur Upadhayay <mayur123a@gmail.com>
2022-12-18 16:50:08 +0530

    feat: Basic example for closure

---
+++ miscellaneous/Closure.ts
@@ -0,0 +1,32 @@
+// An Example to demonstrate data encapsulation and hiding using closures in javascript
+
+const counter = (
+    function () {
+        // data member
+        let privateVar = 0;
+
+        // private method(s)
+        function modifyPrivateVar(delta: number): void {
+            privateVar += delta;
+        }
+
+        // public methods
+        return {
+            increment: function (incFactor: number): void {
+                modifyPrivateVar(incFactor);
+            },
+
+            decrement: function (decFactor: number): void {
+                modifyPrivateVar(decFactor);
+            },
+
+            // returns value of the private variable
+            value: function (): number {
+                return privateVar;
+            }
+        }
+    }
+)();
+
+counter.increment(2);
+console.log(counter.value()); // privateVar = 2
