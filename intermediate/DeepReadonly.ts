584a7bfcfef2a57e63443c9e7dd26872b1ae5e7d
Mayur Upadhayay <mayur123a@gmail.com>
2023-01-16 23:38:11 +0530

    feat: Completed example of Deepreadonly

--- intermediate/DeepReadonly.ts
+++ intermediate/DeepReadonly.ts
@@ -5,3 +5,25 @@
 type DeepReadonly<T> = {
     readonly [K in keyof T]: K extends Object ? DeepReadonly<T[K]> : T[K];
 };
+
+type nestedObjType = {
+    name: {
+        first: 'Mayur'
+        last: 'Upadhayay'
+    },
+    age: 26
+};
+
+type nestedObjReadonly = DeepReadonly<nestedObjType>;
+
+const person: nestedObjReadonly = {
+    name: {
+        first: 'Mayur',
+        last: 'Upadhayay'
+    },
+    age: 26
+};
+
+person.age = 28 // This line throws an error - Cannot assign to 'age' because it is a read-only property.
+
+export { }
