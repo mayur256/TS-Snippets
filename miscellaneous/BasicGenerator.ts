7f4c486cfc60255f64bc59d7e01ce3e06b4d70f6
Mayur Upadhayay <mayur123a@gmail.com>
2023-01-27 23:54:58 +0530

    feat: Adds password generator example

--- miscellaneous/BasicGenerator.ts
+++ miscellaneous/BasicGenerator.ts
@@ -3,7 +3,7 @@
  * @param {number} end 
  * @desc - generator function that generates a range of numbers
  */
-function* generateSequence(start: number, end: number): any {
+export function* generateSequence(start: number, end: number): any {
     for (let i = start; i <= end; i++) {
         yield i;
     }
---
+++ miscellaneous/PasswordGenerator.ts
@@ -0,0 +1,25 @@
+import { generateSequence } from "./BasicGenerator";
+
+const DIGIT = { START: 48, END: 57 };
+const UPPER = { START: 65, END: 90 };
+const LOWER = { START: 97, END: 122 };
+
+// generates a sequence of characters that can be used to create passwords
+function* passwordGenerator() {
+    // generate ascii codes for digits
+    yield* generateSequence(DIGIT.START, DIGIT.END);
+
+    // generates ascii codes for uppercase letters
+    yield* generateSequence(UPPER.START, UPPER.END);
+
+    // generates ascii codes for lowercase letters
+    yield* generateSequence(LOWER.START, LOWER.END);
+};
+
+const codes = passwordGenerator();
+
+for (const code of codes) {
+    console.log(String.fromCharCode(code));
+};
+
+export { };
