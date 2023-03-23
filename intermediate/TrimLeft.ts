573781668f5f4ccf76c9d5135861881c2515eb70
mayur256 <39913092+mayur256@users.noreply.github.com>
2023-03-23 23:47:17 +0530

    fix: Minor fix in trim left example

--- intermediate/TrimLeft.ts
+++ intermediate/TrimLeft.ts
@@ -4,7 +4,8 @@
  * type trimed = TrimLeft<'  Hello World  '> // expected to be 'Hello World  '
  */
 
-type TrimLeft<T extends string> = T extends `${' ' | '\n' | '\t'}${infer X}`
+type WhiteSpace = `${' ' | '\n' | '\t' | '\r'}`
+type TrimLeft<T extends string> = T extends `${WhiteSpace}${infer X}`
     ? TrimLeft<X>
     : T
 
