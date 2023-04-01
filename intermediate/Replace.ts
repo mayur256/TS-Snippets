38d9a761dfcd6173cbee547703f4849ab39e24de
Mayur Upadhayay <mayur123a@gmail.com>
2023-04-01 22:26:06 +0530

    feat: Implemented type example for Replace

---
+++ intermediate/Replace.ts
@@ -0,0 +1,22 @@
+/**
+ * Implement Replace<S, From, To> which replace the string From with To once in the given string S
+ * e.g
+ * type replaced = Replace<'types are fun!', 'fun', 'awesome'> // expected to be 'types are awesome!'
+ */
+
+// Type definition
+type Replace<
+    T extends string,
+    ToBeReplaced extends string,
+    ReplacedWith extends string
+    > = ToBeReplaced extends ''
+    ? T
+    : T extends `${infer Head}${ToBeReplaced}${infer Tail}`
+        ? `${Head}${ReplacedWith}${Tail}`
+        : T
+
+
+// test cases
+type A0 = Replace<'Mayur is good', 'good', 'bad'>
+
+export { }
