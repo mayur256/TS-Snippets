1652ee712de10c8e9b9915194259eaeaaf1a85d5
Mayur Upadhayay <mayur123a@gmail.com>
2023-04-07 23:46:16 +0530

    chore: Minor change to add a node definition

---
+++ DS/LinkedList/LinkedListNode.ts
@@ -0,0 +1,13 @@
+/**
+ * Definition for linkedlist node
+ */
+export default class LinkedListNode {
+    // Data members
+    protected next: any;
+    protected value: any;
+
+    constructor(next: any, value: any) {
+        this.next = next;
+        this.value = value
+    }
+}
