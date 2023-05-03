e229aaa8321a64e7307ba78b869908b7cce50883
Mayur Upadhayay <mayur123a@gmail.com>
2023-05-03 23:31:26 +0530

    chore: Minor refactoring

---
+++ DS/LinkedList/Singly/SinglyLinkedNode.ts
@@ -0,0 +1,13 @@
+/**
+ * Definition for linkedlist node
+ */
+export default class SinglyLinkedListNode {
+    // Data members
+    protected next: any;
+    protected value: any;
+
+    constructor(next: any, value: any) {
+        this.next = next;
+        this.value = value
+    }
+}
