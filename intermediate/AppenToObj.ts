bc75fa460b3069228ac91a63d824ded06b8d482f
mayur256 <39913092+mayur256@users.noreply.github.com>
2023-06-22 23:49:51 +0530

    feat: Implemented basic example for AppendToObject

--- intermediate/AppenToObj.ts
+++ intermediate/AppenToObj.ts
@@ -4,3 +4,18 @@
  * type Test = { id: '1' }
  * type Result = AppendToObject<Test, 'value', 4> // expected to be { id: '1', value: 4 }
  */
+
+// Test cases
+type A = {name: 'mayur', age: 30};
+type AppendedA = AppendToObject<A, 'isBatman', false> // expected {name: 'mayur', age: 30, isBatman: false}
+
+// Type definition
+type AppendToObject<
+    T extends Object,
+    X extends PropertyKey,
+    Y extends any
+> = {
+    [P in keyof T | X]: P extends keyof T ? T[P] : Y
+};
+
+export { }
