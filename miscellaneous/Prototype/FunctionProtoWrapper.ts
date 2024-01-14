2aaa2de1f2f030e9968fe1834ef6f8e69340739f
Mayur Upadhayay <mayur123a@gmail.com>
2024-01-14 16:48:07 +0530

    feat: Adds example of Prototype ovveriding with wrapper

---
+++ miscellaneous/Prototype/FunctionProtoWrapper.ts
@@ -0,0 +1,25 @@
+/**
+ * Modify the Function prototype to include `defer` method that returns a 
+ * wrapper, delaying the function call by ms milliseconds
+ */
+
+interface Function {
+    defer: (ms: number) => (a: number, b: number) => number
+}
+
+// The subject function
+function adder(a: number, b: number): void {
+    console.log(a+b);
+}
+
+// Prototype ovveriding
+Function.prototype.defer = function (ms: number): any {
+    const thisFunc = this;
+    return function (...args: number[]) {
+        setTimeout(() => thisFunc.apply(this, args), ms);
+    }
+}
+
+// Calling overrided prototype method with native arguments
+console.log("Program started");
+adder.defer(1000)(1, 2);
